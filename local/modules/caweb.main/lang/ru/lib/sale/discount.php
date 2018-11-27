c151d052b418fdf501b60a622a8cf81c9108d9cd
pavel <spcell007@gmail.com>
2018-11-27 17:47:20 +0800

    Админ часть готова

---
+++ local/modules/caweb.main/lang/ru/lib/sale/discount.php
@@ -0,0 +1,8 @@
+<?php
+$MESS['TITLE_KEYWORD'] = 'Промо-слово';
+$MESS['TITLE_PRICE_ID'] = 'Тип цены';
+$MESS['TITLE_ACTIVE_FROM'] = 'Дата начала активности';
+$MESS['TITLE_ACTIVE_TO'] = 'Дата окончания активности';
+$MESS['DATA_FROM_ERROR'] = 'Время начала активности должно быть не раньше текущего';
+$MESS['DATA_TO_FROM_ERROR'] = 'Конец активности раньше начала';
+$MESS['DATA_TO_ERROR'] = 'Период активности должен быть не меньше одного дня';
--- local/modules/caweb.main/lib/sale/discount.php
+++ local/modules/caweb.main/lib/sale/discount.php
@@ -3,6 +3,10 @@ namespace Caweb\Main\Sale;
 
 use Bitrix\Main\Entity;
 use Bitrix\Main\Type;
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
 class DiscountTable extends Entity\DataManager {
     public static function getTableName(){
         return "b_caweb_discount";
@@ -10,18 +14,19 @@ class DiscountTable extends Entity\DataManager {
     public static function getConnectionName(){
         return 'default';
     }
-    public static function getMap()
-    {
+    public static function getMap(){
         return array(
             new Entity\IntegerField('ID',array(
                 'primary' => true,
                 'autocomplete' => true
             )),
             new Entity\StringField('KEYWORD',array(
-                'required' => true
+                'required' => true,
+                'title' => Loc::getMessage('TITLE_KEYWORD')
             )),
             new Entity\StringField('PRICE_ID',array(
-                'required' => true
+                'required' => true,
+                'title' => Loc::getMessage('TITLE_PRICE_ID')
             )),
             new Entity\StringField('ACTIVE',array(
                 'required' => true,
@@ -29,16 +34,38 @@ class DiscountTable extends Entity\DataManager {
             )),
             new Entity\DatetimeField('ACTIVE_FROM',array(
                 'required' => true,
-                'default_value' => new Type\DateTime
+                'default_value' => new Type\DateTime,
+                'title' => Loc::getMessage('TITLE_ACTIVE_FROM')
             )),
             new Entity\DatetimeField('ACTIVE_TO',array(
-                'required' => true
+                'required' => true,
+                'title' => Loc::getMessage('TITLE_ACTIVE_TO'),
+                'validation' => function(){
+                    return array(
+                        array(__CLASS__, 'dataValidator')
+                    );
+                }
             )),
             new Entity\ReferenceField(
                 'PRICE',
                 '\Bitrix\Catalog\GroupTable',
                 array('=this.PRICE_ID' => 'ref.ID'))
         );
+    }
+    public static function dataValidator($value, $primary, $row, $field){
+        $dataFrom = $row['ACTIVE_FROM'];
+        $dataTo = $row['ACTIVE_TO'];
+        $thisTime = new Type\DateTime();
+        $timestampFrom = $dataFrom->getTimestamp();
+        $timestampTo = $dataTo->getTimestamp();
+        $timestampThis = $thisTime->getTimestamp();
+        $copyData = clone $dataFrom;
+        $copyData->add("1 day");
+        $timestampDay = $copyData->getTimestamp();
+        if ($timestampThis > $timestampFrom) return Loc::getMessage('DATA_FROM_ERROR');
+        if ($timestampFrom > $timestampTo) return Loc::getMessage('DATA_TO_FROM_ERROR');
+        if ($timestampDay > $timestampTo) return Loc::getMessage('DATA_TO_ERROR');
+        return true;
     }
 
 }
