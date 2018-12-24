2aed3f373c9d27ac86e186bed4f4cb8bf21a50ca
pavel <spcell007@gmail.com>
2018-12-24 15:23:56 +0800

    откатил изменения, перенёс нужные, продолжаю работать над купоном

---
+++ local/modules/caweb.main/lib/log/write.php
@@ -0,0 +1,24 @@
+<?
+namespace Caweb\Main\Log;
+
+use Bitrix\Main\Diag\Debug;
+use Bitrix\Main\IO\File;
+use Bitrix\Main\Type\DateTime;
+
+class Write{
+    const DIR = 'logs/caweb/';
+    const TYPE = '.log';
+    public static function file($name, $var, $rewrite = false){
+        $path = self::DIR.$name.self::TYPE;
+        $timeInt = new DateTime();
+        $time = $timeInt->format('H:i');
+        $timeInt->add('-10 hour');
+        $timeInt = $timeInt->getTimestamp();
+        $file = new File($path);
+        if ($file->isExists() && ($timeInt > $file->getCreationTime()))
+            $file->putContents('');
+        if ($file->isExists() && $rewrite)
+            $file->putContents('');
+        Debug::dumpToFile($var,$time, $path);
+    }
+}
