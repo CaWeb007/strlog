82b5dc9a201e30490d3cbfde4c063b285d3f1f7d
p.reutov <spcell007@gmail.com>
2019-08-15 12:11:37 +0800

    внешняя переделка под кредит онлайн

--- include/footer/copy/pay_system_icons.php
+++ include/footer/copy/pay_system_icons.php
@@ -1,3 +1,5 @@
 <a href="/help/payment/"><i title="MasterCard" class="mastercard"></i>
-<i title="Visa" class="visa"></i>
-	<i title="Mir" class="mir"></i></a>
+    <i title="Visa" class="visa"></i>
+    <i title="Mir" class="mir"></i>
+    <i title="Покупай со Сбербанком" class="sber"></i>
+</a>
