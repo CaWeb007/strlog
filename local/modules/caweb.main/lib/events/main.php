c9d854201ce4012c47e3bca4ca53de70f6b433ac
p.reutov <spcell007@gmail.com>
2019-09-19 13:04:25 +0800

    personal profession in oneClickBuy

--- local/modules/caweb.main/lib/events/main.php
+++ local/modules/caweb.main/lib/events/main.php
@@ -7,7 +7,9 @@ use Caweb\Main\Catalog\Helper as CatalogHelper;
 Loc::loadLanguageFile(__FILE__);
 class Main{
     public static $instance = null;
-    public function OnBeforeUserRegister($arFields){
+    public function OnBeforeUserRegister(&$arFields){
+        if (empty($arFields["PERSONAL_PROFESSION"]))
+            $arFields["PERSONAL_PROFESSION"] = 'КП(ФИЗ)';
         if (($arFields["PERSONAL_PROFESSION"]=="КП(ЮР)") && Helper::notUniqueLegalUser($arFields['UF_INN'], $arFields['UF_KPP'])){
             global $APPLICATION;
             $APPLICATION -> ThrowException(Loc::getMessage('CAWEB_NOT_UNIQUE_LEGAL_USER'));
