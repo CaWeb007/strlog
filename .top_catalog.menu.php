4debb42c8fc9e397b3dd4f96cf920fc61eeea6cc
p.reutov <spcell007@gmail.com>
2019-12-27 09:58:00 +0800

    доработал кастом пункты меню на мобилах

--- .top_catalog.menu.php
+++ .top_catalog.menu.php
@@ -1,15 +1,8 @@
 <?
 $aMenuLinks = Array(
     Array(
-        "Некондиция",
+        "Уцененный товар",
         "/necondition/markdown/",
-        Array(),
-        Array(),
-        ""
-    ),
-    Array(
-        "Прайс-лист",
-        "/price/?filename=%D0%9F%D1%80%D0%B0%D0%B9%D1%81_%D0%9A%D0%9F.XLS",
         Array(),
         Array(),
         ""
