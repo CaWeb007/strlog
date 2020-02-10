984796827c43896a1073bb94f3c3bf3e4dd90d30
p.reutov <spcell007@gmail.com>
2020-02-10 16:41:38 +0800

    описание процесса оформления кредита

--- local/php_interface/include/sale_payment/moneycare/template/form.php
+++ local/php_interface/include/sale_payment/moneycare/template/form.php
@@ -4,13 +4,12 @@ Loc::loadMessages(__FILE__);
 /**@var $params array*/
 ?>
 <div class="money-care-container">
-    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </p>
     <p>
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled
+        Для оформления кредита онлайн:
         <ol>
-            <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </li>
-            <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </li>
-            <li>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled </li>
+            <li>Проверить условия и подтвердить согласие на обработку персональных данных и и заполнить дополнительные поля в короткой анкете.</li>
+            <li>После заполнения короткой заявки, заявка передается на автоматическое рассмотрение в банк за несколько минут с вами свяжется менеджер.</li>
+            <li>При положительном ответе ознакамливаетесь и подписываете электронный комплект документов на кредит.</li>
         </ol>
     </p>
     <a class="form-button" href="<?=$params['CREATE_URL']?>" target="_self"><?=Loc::getMessage('MC_PAY')?></a>
