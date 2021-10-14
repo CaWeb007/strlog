8413861a826c3137356109dd372d8d5cbca6feec
p.reutov <reutov@caweb.ru>
2021-10-14 17:42:54 +0800

    файл включаемой области для caweb:custom.elements

---
+++ include/custom.products_slider.php
@@ -0,0 +1,9 @@
+<?$APPLICATION->IncludeComponent("caweb:custom.elements", "", array(
+	"CUSTOM_FILTER" => "{\"CLASS_ID\":\"CondGroup\",\"DATA\":{\"All\":\"OR\",\"True\":\"True\"},\"CHILDREN\":[{\"CLASS_ID\":\"CondIBElement\",\"DATA\":{\"logic\":\"Equal\",\"value\":[25375,35474]}},{\"CLASS_ID\":\"CondIBSection\",\"DATA\":{\"logic\":\"Equal\",\"value\":2276}}]}",
+		"TITLE" => "Не забудь купить"
+	),
+	false,
+	array(
+	"ACTIVE_COMPONENT" => "N"
+	)
+);?>
