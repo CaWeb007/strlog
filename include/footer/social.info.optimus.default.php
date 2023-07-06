a2e27d3426e9fd266bc260df0009d375b027bf57
p.reutov <reutov@caweb.ru>
2023-07-06 11:50:28 +0800

    добавил телегу

--- include/footer/social.info.optimus.default.php
+++ include/footer/social.info.optimus.default.php
@@ -1,22 +1,22 @@
 <?$APPLICATION->IncludeComponent(
-	"aspro:social.info.optimus", 
-	".default", 
-	array(
+	"aspro:social.info.optimus",
+	".default",
+	Array(
 		"CACHE_GROUPS" => "N",
 		"CACHE_TIME" => "3600000",
 		"CACHE_TYPE" => "A",
+		"COMPONENT_TEMPLATE" => ".default",
 		"COMPOSITE_FRAME_MODE" => "A",
 		"COMPOSITE_FRAME_TYPE" => "AUTO",
-		"TITLE_BLOCK" => "Мы в социальных сетях:",
-		"VK" => "https://vk.com/strlog",
-		"ODN" => "",
 		"FACE" => "",
-		"TWIT" => "",
-		"MAIL" => "",
-		"INST" => "https://www.instagram.com/stroilogistika/",
-		"YOUTUBE" => "https://www.youtube.com/channel/UC96K87yPv9FTK1eEs8KbHVg",
 		"GOOGLE_PLUS" => "",
-		"COMPONENT_TEMPLATE" => ".default"
-	),
-	false
+		"INST" => "",
+		"MAIL" => "",
+		"ODN" => "https://www.ok.ru/stroilogistika",
+		"TITLE_BLOCK" => "Мы в социальных сетях:",
+		"TWIT" => "",
+		"VK" => "https://vk.com/stroilogistika",
+		"YOUTUBE" => "https://www.youtube.com/channel/UC96K87yPv9FTK1eEs8KbHVg",
+        "TELEGRAM" => "https://t.me/stroilogistika"
+	)
 );?>
