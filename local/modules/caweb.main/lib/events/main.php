7af450e9e67e3adde01a1c7b017c9834511fb503
p.reutov <reutov@caweb.ru>
2023-10-02 14:08:11 +0800

    отключаю тестовый режим

--- local/modules/caweb.main/lib/events/main.php
+++ local/modules/caweb.main/lib/events/main.php
@@ -235,7 +235,7 @@ class Main{
         }
         try {
             Loader::includeModule('caweb.main');
-            $ord = new ORD(true);
+            $ord = new ORD();
             $body = array(
                 "name"=> $fields['NAME'],
                 "brand"=> $fields['NAME'],
